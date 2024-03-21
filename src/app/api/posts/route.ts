659a71d716b485d278cea67306b6809e8fe1e74b
eldar Ruslanov <ruslanoveldar16@gmail.com>
2024-03-21 12:59:27 +0600

    eldar ruslanov end api products

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,45 +1,475 @@
 import { NextResponse } from "next/server";
+import Iimage1 from "../../../../public/Mask Group (1).png"
+import Iimage2 from "../../../../public/Mask Group (2).png"
+import Iimage3 from "../../../../public/Mask Group (3).png"
+import Iimage4 from "../../../../public/Mask Group (4).png"
+import Iimage5 from "../../../../public/Mask Group (5).png"
+import Iimage6 from "../../../../public/Mask Group.png"
 
 export async function GET(req: Request) {
     return NextResponse.json([
          {
             id: 1,
-            name: "burger cheez"
+            name: "burger cheez",
+            image: Iimage1
          },
          {
             id: 2,
-            name: "doner"
+            name: "doner",
+            image: Iimage2
          },
          {
             id: 3,
-            name: "big doner"
+            name: "big doner",
+            image: Iimage3
          },
          {
             id: 4,
-            name: "big burger"
+            name: "big burger",
+            image: Iimage4
          },
          {
             id: 5,
-            name: "vodka"
+            name: "vodka",
+            image: Iimage5
          },
          {
             id: 6,
-            name: "Coca Cola"
+            name: "Coca Cola",
+            image: Iimage6
          },
          {
             id: 7,
-            name: "Fanta"
+            name: "Fanta",
+            image: Iimage1
         },
         {
             id: 8,
-            name: "Sprite"
+            name: "Sprite",
+            image: Iimage2
         },
         {
             id: 9,
-            name: "shaurma"
+            name: "shaurma",
+            image: Iimage3
         },
         {
             id: 10,
+            name: "ramen",
+            image: Iimage4
+        },
+        {
+            id: 11,
+            name: "ramen",
+            image: Iimage5
+        },
+        {
+            id: 12,
+            name: "ramen",
+            image: Iimage6
+        },
+        {
+            id: 13,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 14,
+            name: "ramen",
+            image: Iimage2
+        },
+        {
+            id: 15,
+            name: "ramen",
+            image: Iimage3
+        },
+        {
+            id: 16,
+            name: "ramen",
+            image: Iimage4
+        },
+        {
+            id: 17,
+            name: "ramen",
+            image: Iimage5
+        },
+        {
+            id: 18,
+            name: "ramen",
+            image: Iimage6
+        },
+        {
+            id: 19,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 20,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 21,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 22,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 23,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 24,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 25,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 26,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 27,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 28,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 29,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 30,
+            name: "ramen",
+            image: Iimage1
+        },
+        {
+            id: 31,
+            name: "ramen",
+            image: Iimage2
+        },{
+            id: 32,
+            name: "ramen",
+            image: Iimage2
+        },
+        {
+            id: 33,
+            name: "ramen",
+            image: Iimage2
+        },
+        {
+            id: 34,
+            name: "ramen",
+            image: Iimage2
+        },
+        {
+            id: 35,
+            name: "ramen",
+            image: Iimage2
+        },
+        {
+            id: 36,
+            name: "ramen"
+        },
+        {
+            id: 37,
+            name: "ramen"
+        },
+        {
+            id: 38,
+            name: "ramen"
+        },
+        {
+            id: 39,
+            name: "ramen"
+        },
+        {
+            id: 40,
+            name: "ramen"
+        },
+        {
+            id: 41,
+            name: "ramen"
+        },
+        {
+            id: 42,
+            name: "ramen"
+        },
+        {
+            id: 43,
+            name: "ramen"
+        },
+        {
+            id: 44,
+            name: "ramen"
+        },
+        {
+            id: 45,
+            name: "ramen"
+        },
+        {
+            id: 46,
+            name: "ramen"
+        },
+        {
+            id: 47,
+            name: "ramen"
+        },
+        {
+            id: 48,
+            name: "ramen"
+        },
+        {
+            id: 49,
+            name: "ramen"
+        },
+        {
+            id: 50,
+            name: "ramen"
+        },
+        {
+            id: 51,
+            name: "ramen"
+        },
+        {
+            id: 52,
+            name: "ramen"
+        },
+        {
+            id: 53,
+            name: "ramen"
+        },
+        {
+            id: 54,
+            name: "ramen"
+        },
+        {
+            id: 55,
+            name: "ramen"
+        },
+        {
+            id: 56,
+            name: "ramen"
+        },
+        {
+            id: 57,
+            name: "ramen"
+        },
+        {
+            id: 58,
+            name: "ramen"
+        },
+        {
+            id: 59,
+            name: "ramen"
+        },
+        {
+            id: 60,
+            name: "ramen"
+        },
+        {
+            id: 61,
+            name: "ramen"
+        },
+        {
+            id: 62,
+            name: "ramen"
+        },
+        {
+            id: 63,
+            name: "ramen"
+        },
+        {
+            id: 64,
+            name: "ramen"
+        },
+        {
+            id: 65,
+            name: "ramen"
+        },
+        {
+            id: 66,
+            name: "ramen"
+        },
+        {
+            id: 67,
+            name: "ramen"
+        },{
+            id: 68,
+            name: "ramen"
+        },
+        {
+            id: 69,
+            name: "ramen"
+        },
+        {
+            id: 70,
+            name: "ramen"
+        },
+        {
+            id: 71,
+            name: "ramen"
+        },
+        {
+            id: 72,
+            name: "ramen"
+        },
+        {
+            id: 73,
+            name: "ramen"
+        },
+        {
+            id: 74,
+            name: "ramen"
+        },
+        {
+            id: 75,
+            name: "ramen"
+        },
+        {
+            id: 76,
+            name: "ramen"
+        },
+        {
+            id: 77,
+            name: "ramen"
+        },
+        {
+            id: 78,
+            name: "ramen"
+        },
+        {
+            id: 79,
+            name: "ramen"
+        },
+        {
+            id: 80,
+            name: "ramen"
+        },{
+            id: 81,
+            name:"ramen"
+        },
+        {
+            id:82,
+            name: "ramen"
+        },
+        {
+            id: 83,
+            name: "ramen"
+        },
+        {
+            id: 84,
+            name: "ramen"
+        },
+        {
+            id: 85,
+            name: "ramen"
+        },
+        {
+            id: 86,
+            name: "ramen"
+        },
+        {
+            id: 87,
+            name: "ramen"
+        },
+        {
+            id: 88,
+            name: "ramen"
+        },
+        {
+            id: 89,
+            name: "ramen"
+        },
+        {
+            id: 90,
+            name: "ramen"
+        },
+        {
+            id: 91,
+            name: "ramen"
+        },
+        {
+            id: 92,
+            name: "ramen"
+        },
+        {
+            id: 93,
+            name: "ramen"
+        },
+        {
+            id: 94,
+            name: "ramen"
+        },
+        {
+            id: 95,
+            name: "ramen"
+        },
+        {
+            id: 96,
+            name: "ramen"
+        },
+        {
+            id: 97,
+            name: "ramen"
+        },
+        {
+            id: 98,
+            name: "ramen"
+        },
+        {
+            id: 99,
+            name: "ramen"
+        },
+        {
+            id: 100,
+            name: "ramen"
+        },
+        {
+            id: 101,
+            name: "ramen"
+        },
+        {
+            id: 102,
+            name: "ramen"
+        },
+        {
+            id: 103,
+            name: "ramen"
+        },
+        {
+            id: 104,
+            name: "ramen"
+        },
+        {
+            id: 105,
+            name: "ramen"
+        },
+        {
+            id: 106,
+            name: "ramen"
+        },
+        {
+            id: 107,
+            name: "ramen"
+        },
+        {
+            id: 108,
             name: "ramen"
         }
         ])
